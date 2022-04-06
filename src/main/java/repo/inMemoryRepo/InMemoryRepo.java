d66d7ad8f3ca6787f11688ca7839d50952de4477
Liviu-Terebes <tlie3028@scs.ubbcluj.ro>
2022-04-06 16:19:55 +0300

    Added repo

---
+++ src/main/java/repo/inMemoryRepo/InMemoryRepo.java
@@ -0,0 +1,72 @@
+package repo.inMemoryRepo;
+
+import main.java.exception.ValidatorException;
+import main.java.model.BaseEntity;
+import main.java.repo.Repository;
+import main.java.validator.IValidator;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+/**
+ * @author Liviu
+ * InMemoryRepo class (for storing elements in Memory)
+ * This class implements the Repo Interface
+ */
+
+
+public class InMemoryRepo<ID, T extends BaseEntity<ID>> implements Repository<ID, T> {
+
+    private Map<ID, T> entities;
+    private IValidator<T> validator;
+
+    public InMemoryRepo(IValidator<T> validator) {
+        this.validator = validator;
+        entities = new HashMap<>();
+    }
+
+    @Override
+    public Optional<T> findOne(ID id) {
+        if (id == null) {
+            throw new IllegalArgumentException("id must not be null");
+        }
+        return Optional.ofNullable(entities.get(id));
+    }
+
+    @Override
+    public Iterable<T> findAll() {
+        Set<T> allEntities = entities.entrySet().stream().map(entry -> entry.getValue()).collect(Collectors.toSet());
+        return allEntities;
+    }
+
+    @Override
+    public Optional<T> save(T entity) throws ValidatorException {
+        if (entity == null) {
+            throw new IllegalArgumentException("id must not be null");
+        }
+        validator.validate(entity);
+        return Optional.ofNullable(entities.putIfAbsent(entity.getId(), entity));
+    }
+
+    @Override
+    public Optional<T> delete(ID id) {
+        if (id == null) {
+            throw new IllegalArgumentException("id must not be null");
+        }
+        return Optional.ofNullable(entities.remove(id));
+    }
+
+    @Override
+    public Optional<T> update(T entity) throws ValidatorException {
+        if (entity == null) {
+            throw new IllegalArgumentException("entity must not be null");
+        }
+        validator.validate(entity);
+        return Optional.ofNullable(entities.computeIfPresent(entity.getId(), (k, v) -> entity));
+    }
+}
+
