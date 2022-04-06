a1cc5ad51b87f8fa301ddb552bc23aa071aa2f86
917SusciucAnastasia <susciuc.anastasia@gmail.com>
2022-04-06 16:13:17 +0300

    Add Entity

---
+++ src/main/java/model/BaseEntity.java
@@ -0,0 +1,31 @@
+package main.java.model;
+
+/**
+ * Entity Base class
+ * Client, Rental and Movie
+ * will inherit from Entity
+ *
+ * @author Ani.
+ *
+ */
+
+
+public class BaseEntity<ID> {
+    private ID id;
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+}
+
