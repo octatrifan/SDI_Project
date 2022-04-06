4f80bb16cf9f19bd65f0a65d0f98d846e3c1b835
Tasadan Filip <tasadan.filip@gmail.com>
2022-04-06 16:21:22 +0300

    Validator and exception addded

---
+++ src/main/java/exception/ValidatorException.java
@@ -0,0 +1,23 @@
+package exception;
+
+/**
+ * Base Validator exception class.
+ *
+ * @author Dani
+ *
+ *
+ */
+
+public class ValidatorException extends CarRentalsAppException {
+    public ValidatorException(String message) {
+        super(message);
+    }
+
+    public ValidatorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ValidatorException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/validator/RentalValidator.java
@@ -0,0 +1,39 @@
+package validator;
+
+import exception.ValidatorException;
+import model.Rental;
+
+import java.util.Optional;
+
+/**
+ * Rental Validator interface.
+ *
+ * @author Dani
+ *
+ *
+ */
+
+public class RentalValidator implements IValidator<Rental> {
+    @Override
+    public void validate(Rental rental) throws ValidatorException {
+        Optional.of(rental.getId())
+                .filter(x -> x <= 0)
+                .ifPresent(s -> { throw new ValidatorException("Error: Found negative ID for rental!"); });
+
+        Optional.of(rental.getClientID())
+                .filter(x -> x <= 0)
+                .ifPresent(s -> { throw new ValidatorException("Error: Found negative ID for rental client!"); });
+
+        Optional.of(rental.getMovieID())
+                .filter(x -> x <= 0)
+                .ifPresent(s -> { throw new ValidatorException("Error: Found negative ID for rental movie!"); });
+
+        Optional.of(rental.getId())
+                .filter(x -> x <= 0)
+                .ifPresent(s -> { throw new ValidatorException("Error: Found negative ID for rental!"); });
+
+        Optional.of(rental.getRentDate())
+                .filter(x -> x.after(rental.getDeadlineDate()))
+                .ifPresent(s -> { throw new ValidatorException("Error: Rent date must be after deadline date!"); });
+    }
+}
