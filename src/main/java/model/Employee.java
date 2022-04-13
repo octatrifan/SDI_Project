56cf9a5fb6ac162ca57170de9907a89267588a77
Tasadan Filip <tasadan.filip@gmail.com>
2022-04-13 15:46:14 +0300

    Model classes added

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,102 @@
+package model;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * Employee class
+ * @author Liviu
+ */
+
+public class Employee extends BaseEntity<Integer> {
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private String email;
+    private int salary;
+
+    public Employee() {}
+
+    // Constructor
+    public Employee(String firstName, String lastName, Date birthDate, String email, int salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.email = email;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Number getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (!Objects.equals(firstName, employee.firstName)) return false;
+        if (!Objects.equals(lastName, employee.firstName)) return false;
+        return birthDate.equals(employee.birthDate);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName.hashCode();
+        result = 31 * result + lastName.hashCode();
+        result = 31 * result + birthDate.hashCode();
+        result = 31 * result + email.hashCode();
+        result = 31 * result + Integer.valueOf(salary).hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthDate=" + birthDate +
+                ", email='" + email + '\'' +
+                ", salary='" + salary + '\'' +
+                '}';
+    }
+}
